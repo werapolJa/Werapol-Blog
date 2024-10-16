0d1c75b3e68f411c21aa2d6cf2baa2e2b0e2c603
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-16 15:08:09 +0700

    feat:add border and font-size navber

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import heroimage from "./assets/heroimg.jpg";
 function Navbar() {
   return (
     <>
-      <nav className="max-w-7xl mx-auto h-[80px] flex items-center">
+      <nav className="max-w-7xl mx-auto h-[80px] flex items-center ">
         <div className="flex justify-between w-full">
-          <div className=" text-3xl">hh.</div>
+          <div className=" text-3xl font-semibold">
+            hh<span className="text-green-400">.</span>
+          </div>
           <div className="flex gap-3">
             <button className="border-black border-2 w-[127px] h-[48px] rounded-full hover:cursor-pointer">
               Log in
@@ -16,6 +18,7 @@ function Navbar() {
           </div>
         </div>
       </nav>
+      <div className="border-b-2 "></div>
     </>
   );
 }
