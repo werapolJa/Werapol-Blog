4526736c821c8e7a50dc7a3806397773c66e90ad
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-16 14:31:03 +0700

    feat:navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import "./App.css";
+import heroimage from "./assets/heroimg.jpg";
+function Navbar() {
+  return (
+    <>
+      <nav className="max-w-7xl mx-auto h-[80px] flex items-center">
+        <div className="flex justify-between w-full">
+          <div className=" text-3xl">hh.</div>
+          <div className="flex gap-3">
+            <button className="border-black border-2 w-[127px] h-[48px] rounded-full hover:cursor-pointer">
+              Log in
+            </button>
+            <button className="w-[127px] h-[48px] rounded-full bg-black text-white hover:cursor-pointer">
+              Sign up
+            </button>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
 
 function App() {
   return (
     <>
-      <section className="flex justify-center items-center h-screen">
-        <h1>My First React App on Vite</h1>
-      </section>
+      <Navbar />
     </>
   );
 }
