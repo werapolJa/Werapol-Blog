2b837a61533dc3a62ab630fefb2b87fe1eb66778
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-16 17:03:23 +0700

    feat:add burgerbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import heroimage from "./assets/heroimg.jpg";
 function Navbar() {
   return (
     <>
-      <nav className="max-w-7xl mx-auto h-[80px] flex items-center ">
-        <div className="flex justify-between w-full">
+      <nav className="lg:max-w-7xl sm:mx-auto mx-10 h-[80px] flex items-center ">
+        <div className="flex justify-between w-full items-center">
           <div className=" text-3xl font-semibold">
             hh<span className="text-green-400">.</span>
           </div>
-          <div className="flex gap-3">
+          <div className="inline md:hidden">
+            <div className="">
+              <img
+                width="20"
+                height="20"
+                src="https://img.icons8.com/ios/50/menu--v1.png"
+                alt="menu--v1"
+              />
+            </div>
+          </div>
+          <div className="hidden md:flex gap-3 ">
             <button className="border-black border-2 w-[127px] h-[48px] rounded-full hover:cursor-pointer">
               Log in
             </button>
