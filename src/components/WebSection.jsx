ad37c4610fdac048c0bfe8dda4b32432c6eb526f
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-23 19:25:03 +0700

    feat:add render blogcard  in articlesesection

--- src/components/WebSection.jsx
+++ src/components/WebSection.jsx
@@ -64,7 +64,7 @@ export function HeroSection(props) {
           </div>
           <div className="">
             <h5>-Author</h5>
-            <h3 className="text-[24px] font-[600] my-3">Thompson P.</h3>
+            <h3 className="text-[24px] font-[600] my-3">{}</h3>
             <h4 className="mb-8 text-[18px] w-[347px]">
               I am a pet enthusiast and freelance writer who specializes in
               animal behavior and care. With a deep love for cats, I enjoy
@@ -122,11 +122,6 @@ export function Footer() {
 export function ArticleSection() {
   let [navBarHead, setNavBarHead] = useState("Highlight");
 
-  console.log(navBarHead);
-
-  let we = blogPosts.filter((an) => an.date < 5);
-  console.log(we);
-
   return (
     <>
       <div className="flex justify-center lg:max-w-7xl lg:mx-auto  lg:justify-start">
@@ -141,7 +136,7 @@ export function ArticleSection() {
               navBarHead === "Highlight"
                 ? `bg-[#DAD6D1] rounded-xl p-3 cursor-pointer`
                 : `px-3 ` +
-                  `hover:cursor-pointer hover:bg-[#e7e5e3] rounded-xl `
+                  `hover:cursor-pointer hover:bg-[#e7e5e3] rounded-xl p-3 `
             }
             onClick={() => setNavBarHead("Highlight")}
           >
@@ -218,52 +213,58 @@ export function ArticleSection() {
           </select>
         </div>
       </div>
-      <BlogCard />
+      <div className="max-w-7xl mx-auto my-10">
+        <div className="lg:grid lg:grid-cols-2 gap-8">
+          {navBarHead === "Highlight"
+            ? blogPosts.map((animal) => <BlogCard animal={animal} />)
+            : blogPosts
+                .filter((animalfilter) => animalfilter.category == navBarHead)
+                .map((animal) => <BlogCard animal={animal} />)}
+        </div>
+      </div>
     </>
   );
 }
 
-export function BlogCard() {
+export function BlogCard({ animal }) {
+  console.log(animal);
+
   return (
-    <div className="max-w-7xl mx-auto my-10">
-      <div className="lg:grid lg:grid-cols-2 gap-8">
-        {blogPosts.map((animal, index) => (
-          <div className="flex flex-col gap-4" key={index}>
-            <a href="#" className="relative h-[212px] sm:h-[360px]">
-              <img
-                className="w-full h-full object-cover rounded-md"
-                src={animal.image}
-                alt="Understanding Cat Behavior: Why Your Feline Friend Acts the Way They Do"
-              />
-            </a>
-            <div className="flex flex-col">
-              <div className="flex">
-                <span className="bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mb-2">
-                  {animal.category}
-                </span>
-              </div>
-              <a href="#">
-                <h2 className="text-start font-bold text-xl mb-2 line-clamp-2 hover:underline">
-                  {animal.title}
-                </h2>
-              </a>
-              <p className="text-muted-foreground text-sm mb-4 flex-grow line-clamp-3">
-                {animal.description}
-              </p>
-              <div className="flex items-center text-sm">
-                <img
-                  className="w-8 h-8 rounded-full mr-2"
-                  src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
-                  alt="Tomson P."
-                />
-                <span>{animal.author}</span>
-                <span className="mx-2 text-gray-300">|</span>
-                <span>{animal.date}</span>
-              </div>
-            </div>
+    <>
+      <div className="flex flex-col gap-4" key={animal.id}>
+        <a href="#" className="relative h-[212px] sm:h-[360px]">
+          <img
+            className="w-full h-full object-cover rounded-md"
+            src={animal.image}
+            alt="Understanding Cat Behavior: Why Your Feline Friend Acts the Way They Do"
+          />
+        </a>
+        <div className="flex flex-col">
+          <div className="flex">
+            <span className="bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mb-2">
+              {animal.category}
+            </span>
           </div>
-        ))}
+          <a href="#">
+            <h2 className="text-start font-bold text-xl mb-2 line-clamp-2 hover:underline">
+              {animal.title}
+            </h2>
+          </a>
+          <p className="text-muted-foreground text-sm mb-4 flex-grow line-clamp-3">
+            {animal.description}
+          </p>
+          <div className="flex items-center text-sm">
+            <img
+              className="w-8 h-8 rounded-full mr-2"
+              src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg"
+              alt="Tomson P."
+            />
+            <span>{animal.author}</span>
+            <span className="mx-2 text-gray-300">|</span>
+            <span>{animal.date}</span>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
