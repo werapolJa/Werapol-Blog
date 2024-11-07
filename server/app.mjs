0bc052a34f6773e7886e94fce31150b7e0b9de54
werapol <wesakaber@gmail.com>
2024-11-07 21:12:39 +0700

    fix:test server

---
+++ server/app.mjs
@@ -0,0 +1,30 @@
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/profiles",(req,res)=>{
+    return res.json({
+        "data":  {
+            "name": "john",
+            "age": 20
+        }
+      
+    })
+})
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello TechUp!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at ${port}`);
+});
